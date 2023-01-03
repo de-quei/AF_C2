7f1af1dd4f9035f97990826f4f65634b24533107
Hyunji Kim <108276366+de-quei@users.noreply.github.com>
2023-01-03 15:06:05 +0900

    add Animation

--- Animation.cpp
+++ Animation.cpp
@@ -1,17 +1,25 @@
 #include<SFML/Graphics.hpp>
+#include<stdlib.h>
+#include<time.h>
 
 using namespace sf;
 
 struct Player {
 	RectangleShape sprite;
 	int fps;                 // Frames per sec
-	int idx;                // 애니메이션 index
+	int idx;                 // 애니메이션 index
+	int frame_num;           // 애니메이션 frame수
+	long ani_time;           // 애니메이션이 바뀔 때의 시간
+
 };
 
 int main()
 {
-	RenderWindow window(VideoMode(1200, 800), "Card Game!");
+	RenderWindow window(VideoMode(1200, 800), "Animation");
 	window.setFramerateLimit(60);
+
+	long start_time;
+	long spent_time;
 
 	Texture run[10];
 	run[0].loadFromFile("./resources/png/Run__000.png");
@@ -27,12 +35,18 @@ int main()
 
 	struct Player player;
 	player.fps = 10;
+	player.frame_num = 10;
+	player.idx = 0;
 	player.sprite.setTexture(&run[0]);
 	player.sprite.setSize(Vector2f(90, 120));
 	player.sprite.setPosition(200, 600);
 
+	start_time = clock();
+	player.ani_time = start_time;
+
 	while (window.isOpen())
 	{
+		spent_time = clock() - start_time;
 		Event event;
 		while (window.pollEvent(event))
 		{
@@ -47,6 +61,14 @@ int main()
 			}
 		}
 
+		// 0.1초마다 애니메이션 그림이 바뀜
+		while (spent_time - player.ani_time > 1000 / player.frame_num);
+		{
+			player.ani_time = spent_time;
+			player.sprite.setTexture(&run[player.idx % player.frame_num]);
+			player.idx++;
+		}
+		
 		window.clear(Color::Magenta);
 
 		window.draw(player.sprite);
